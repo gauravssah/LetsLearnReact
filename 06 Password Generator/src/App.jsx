29954d7d4cf57ca54adf8728d8b58ffef1f8c4c1
gauravssah <124705806+gauravssah@users.noreply.github.com>
2024-04-07 01:32:37 +0530

    password generator

--- 06 Password Generator/src/App.jsx
+++ 06 Password Generator/src/App.jsx
@@ -1,33 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+
+import { useState, useCallback, useEffect, useRef } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [length, setLength] = useState(7);
+  const [nummberAllowed, setNummberAllowed] = useState(false);
+  const [specialCharAllowed, setSpecialCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const generatePassword = useCallback(() => {
+
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (nummberAllowed) str += "1234567890";
+    if (specialCharAllowed) str += "~!@#$%^&*_-";
+
+    for (let i = 1; i <= length; i++) {
+      let ch = Math.floor(Math.random() * str.length + 1);
+
+      pass += str.charAt(ch);
+
+    }
+
+    setPassword(pass);
+
+  }, [length, nummberAllowed, specialCharAllowed, setPassword])
+
+  useEffect(() => {
+    generatePassword()
+  }, [length, nummberAllowed, specialCharAllowed, generatePassword])
+
+
+  const passwordRef = useRef(null);
+
+  // const copyPassword = () => {
+  //   navigator.clipboard.writeText(password)
+  //     .then(() => {
+  //       alert("Password copied to clipboard");
+  //     })
+  //     .catch((err) => {
+  //       console.error('Could not copy text: ', err);
+  //     });
+  // }
+
+  const copyPassword = useCallback(() => {
+    passwordRef.current?.select();
+    // passwordRef.current?.setSelectionRange(0, max);
+
+    window.navigator.clipboard.writeText(password);
+  }, [password])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="containers">
+        <h1>Password Generator</h1>
+
+        <div className="passwordContainer">
+          <input type="text" placeholder="Password" readOnly value={password} ref={passwordRef} />
+          <button className="copybtn" onClick={copyPassword} >Copy</button>
+        </div>
+
+        <div className="dependency">
+          <input type="range" min={1} max={26} value={length} onChange={(e) => setLength(e.target.value)} />
+          <p>Length : ( {length} )</p>
+
+          <input type="checkbox" name="number" id="number" defaultChecked={nummberAllowed} onChange={() => setNummberAllowed((prev) => !prev)} />
+          <label htmlFor="number">Number</label>
+
+          <input type="checkbox" name="Characters" id="Characters" defaultChecked={specialCharAllowed} onChange={() => setSpecialCharAllowed((prev) => !prev)} />
+          <label htmlFor="Characters">Characters</label>
+        </div>
+
+        <div className="github">
+          <h2>Code with 💖 on <a href="https://github.com/gauravssah" target="_blank" >GitHub</a> </h2>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
