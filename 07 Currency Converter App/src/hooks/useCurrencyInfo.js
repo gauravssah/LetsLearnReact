c846e833f22daf3af215105b69e94bec43eb28cb
gauravssah <124705806+gauravssah@users.noreply.github.com>
2024-04-11 01:00:05 +0530

    some chanh

--- 07 Currency Converter App/src/hooks/useCurrencyInfo.js
+++ 07 Currency Converter App/src/hooks/useCurrencyInfo.js
@@ -9,7 +9,7 @@ function useCurrencyInfo(currency) {
             .then((res) => res.json())
             .then((res) => setData(res[currency]))
 
-        console.log(data);
+        // console.log(data);
     }, [currency])
     // console.log(data);
 
