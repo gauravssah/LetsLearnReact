e92504e6e349a1af4219bae07f2526fac5091a6b
gauravssah <124705806+gauravssah@users.noreply.github.com>
2024-04-18 09:16:25 +0530

    state store configurd

--- 13 MegeBlog/src/App.jsx
+++ 13 MegeBlog/src/App.jsx
@@ -1,14 +1,41 @@
 import './App.css'
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>A blog app in using app write:</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className=' min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className=' w-full block'>
+        <Header />
+        <main>
+          Todo:  {/* <Outlet/> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
+
 }
 
 export default App
